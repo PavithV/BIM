e7b5e4c28ec51eeea2282fe10c22402c1c863839
Pavith Vickneswararajah <vpavith02@gmail.com>
2025-10-14 15:18:48 +0000

    Wenn ich mich einloggen will lädt es einfach nur

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,15 +1,25 @@
 // src/app/api/auth/logout/route.ts
-import { NextResponse } from 'next/server';
+import { getAdminAuth } from '@/firebase/admin';
+import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST() {
+export async function POST(request: NextRequest) {
   try {
+    const sessionCookie = cookies().get('__session')?.value;
+    if (sessionCookie) {
+        const decodedClaims = await getAdminAuth().verifySessionCookie(sessionCookie).catch(() => null);
+        if(decodedClaims) {
+            await getAdminAuth().revokeRefreshTokens(decodedClaims.sub);
+        }
+    }
+
     const options = {
       name: '__session',
       value: '',
       maxAge: -1,
     };
 
-    const response = NextResponse.json({}, { status: 200 });
+    const response = NextResponse.json({success: true}, { status: 200 });
     response.cookies.set(options);
     return response;
   } catch (error) {
